66fe23fe9a2496e0d51eb0f4e331835f0b4d87bd
rodrigmars <rodrigmars@gmail.com>
2023-02-11 16:51:34 -0300

    Ajustes para criação de tupla

--- pipegencode/app.py
+++ pipegencode/app.py
@@ -1,40 +1,53 @@
-from typing import Generator
+from typing import Generator, Tuple
 from random import randint
 from time import sleep
 
+def create(code, data: list) -> list:
 
-def main(mock_data: list):
+    return [code, data[0], data[1]]
 
-    def create(data: list) -> int:
-        mock_data.append([code := mock_data[-1][0] + 1, data[0], data[1]])
-        return code
 
-    def find_by_email(id: int) -> tuple:
-        return list(filter(lambda x: x[0] == id, mock_data))[0],
+def find_by_email(data: Tuple[int, list]) -> tuple:
 
-    def email(_) -> set:
-        return {1, "Email disparado"}
+    id, mock_data = data
 
-    def pipe_generator(fns: tuple) -> Generator:
+    return list(filter(lambda x: x[0] == id, mock_data))[0],
 
-        for fn in fns:
 
-            sleep(randint(2, 5))
+def email(_) -> set:
+    return {1, "Email disparado"}
 
-            yield fn
+def pipe_generator(fns: tuple) -> Generator:
 
-    data = ('lkc@452.com', True)
+    for fn in fns:
 
-    for output in pipe_generator((create, find_by_email, email)):
+        sleep(randint(2, 5))
 
-        print(data := output(data))
+        yield fn
 
-    print("\n", mock_data)
 
-if __name__ == "__main__":
+def main():
 
-    main([[10, 'Tke', False],
+    mock_data = [[10, 'Tke', False],
           [14, 'tk_@452.com', True],
           [18, 'e4_@155.com', False],
           [22, 'op_@785.com', True],
-          [25, 'rt_@89.com', False]])
+          [25, 'rt_@89.com', False]]
+
+    data = ('lkc@452.com', True)
+
+    for i, output in enumerate(pipe_generator((create, find_by_email, email))):
+
+        if i < 1:
+            mock_data.append(output(mock_data[-1][0] + 1, data))
+            
+            data = mock_data[-1][0], mock_data
+            
+            print(f"\npipe:{i}", data, "\n")
+
+        else:
+            print(f"pipe:{i}", data := output(data), "\n")
+
+if __name__ == "__main__":
+
+    main()
